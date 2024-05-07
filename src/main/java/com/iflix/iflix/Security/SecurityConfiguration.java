6622f427813f91e9851239d150fff56f56449814
Vu Hoang Long <vuhlong1412@gmail.com>
2024-05-07 21:01:52 +0700

    security config - authentication with jwt

--- src/main/java/com/iflix/iflix/Security/SecurityConfiguration.java
+++ src/main/java/com/iflix/iflix/Security/SecurityConfiguration.java
@@ -1,25 +1,64 @@
 package com.iflix.iflix.Security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 public class SecurityConfiguration {
+
+    private final String[] PUBLIC_ENDPOINTS =
+            {"/users","/auth/token","/auth/introspect"};
+
+    @Value("${jwt.signerKey}")
+    private String signerKey;
 
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-//        httpSecurity.authorizeHttpRequests(
-//                configurer -> configurer
-//                        .requestMatchers("/auth/**").authenticated()
-//                        .anyRequest().permitAll()
-//        );
-//        return httpSecurity.build();
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeHttpRequests(
+                configurer -> configurer
+                        .requestMatchers( HttpMethod.POST,PUBLIC_ENDPOINTS).permitAll()
+                        .anyRequest().authenticated()
+        );
+
+        //validate token
+        httpSecurity.oauth2ResourceServer(oauth2 ->
+                oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder()))
+        );
+
+        //validate
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+        return httpSecurity.build();
+    }
+
+
+    //cung cap signerKey va algorithm
+    @Bean
+    public JwtDecoder jwtDecoder(){
+        SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(),"HS512");
+        return NimbusJwtDecoder
+                .withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build()
+                ;
+    }
 }
